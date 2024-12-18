5c6a72d5a71db3bdac557334a83f957614d4f44a
Diones <pereiradiones987@gmail.com>
2024-12-17 21:08:15 -0300

    page home

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,18 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
   @Get()
   @Render('index')
-  home() {
-    /*
-      const users = this.appService.findAll();
-      return { users };
-    */
-    return;
-  }
+  home() {}
 }
